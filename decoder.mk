3eef8c3e4edb9b710653d310bca2dd33d7bff49a
Z-EBRA1 <ebna16@gmail.com>
2025-06-01 16:07:36 +0200

    Update decoder.mk

--- decoder.mk
+++ decoder.mk
@@ -9,29 +9,33 @@ $(PKG_LOCALSOURCE_PACKAGE)
 $(PKG_CONFIGURED_NOP)
 
 $($(PKG)_BINARY): $($(PKG)_DIR)/.configured
-	$(SUBMAKE) -C $(DECODER_DIR) \
-		CC="$(TARGET_CC)" \
-		CFLAGS="$(TARGET_CFLAGS)" \
-		OPT="" \
-		OPENSSL=$(if $(FREETZ_PACKAGE_DECODER_LIBCRYPTO),y,n) \
-		STATIC=$(if $(FREETZ_PACKAGE_DECODER_DYNAMIC),n,y) \
-		NETTLE_HOST=$(TARGET_GNU_TRIPLET) \
-		NETTLE_TARGET=$(REAL_GNU_TARGET_NAME) \
-		NETTLE_BUILD=$(GNU_HOST_NAME)
+    $(SUBMAKE) -C $(DECODER_DIR) \
+        CC="$(TARGET_CC)" \
+        CFLAGS="$(TARGET_CFLAGS)" \
+        OPT="" \
+        OPENSSL=$(if $(FREETZ_PACKAGE_DECODER_LIBCRYPTO),y,n) \
+        STATIC=$(if $(FREETZ_PACKAGE_DECODER_DYNAMIC),n,y) \
+        NETTLE_HOST=$(TARGET_GNU_TRIPLET) \
+        NETTLE_TARGET=$(REAL_GNU_TARGET_NAME) \
+        NETTLE_BUILD=$(GNU_HOST_NAME)
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
-	$(INSTALL_BINARY_STRIP)
+    $(INSTALL_BINARY_STRIP)
 
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY) 
 
 $(pkg)-clean:
-	-$(SUBMAKE) -C $(DECODER_DIR) clean
+    -$(SUBMAKE) -C $(DECODER_DIR) clean
 
 $(pkg)-uninstall:
-	$(SUBMAKE) -C $(DECODER_DIR) \
-		bindir="$(dir $@)" \
-		uninstall
+    $(SUBMAKE) -C $(DECODER_DIR) \
+        bindir="$(dir $@)" \
+        uninstall
 
 $(PKG_FINISH)
+
+
+
+
